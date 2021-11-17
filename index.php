82ff09f554fc117b1583715c3bc19a9dfb140aaf
rubbyguntara <distortion4by@gmail.com>
2021-11-17 12:45:15 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde amet qui consectetur quae! Ipsam, odit facere. Nam atque eius doloremque consectetur. Ea dolore pariatur architecto omnis sunt odit, quisquam vel!</p>
 </body>
 </html>
