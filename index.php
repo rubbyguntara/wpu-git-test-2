895ed4677b95011b3a4065b0c653eb0c6eb287a8
rubbyguntara <distortion4by@gmail.com>
2021-11-17 12:58:22 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde amet qui consectetur quae! Ipsam, odit facere. Nam atque eius doloremque consectetur. Ea dolore pariatur architecto omnis sunt odit, quisquam vel!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
+
 </html>
